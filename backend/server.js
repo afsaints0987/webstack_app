aa0f70b5871111c75cc94d055f560b6bead6fdee
Aaron Fritz Santos <Aaron.Santos@EntainGroup.com>
2022-08-17 17:37:47 +0800

    add database

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
 const app = express()
 
 app.use(express.json())
@@ -12,3 +14,5 @@ app.use('/api/tasks', require('./routes/taskRoutes'))
 app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
+
+connectDB()
